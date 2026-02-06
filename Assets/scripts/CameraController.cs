c6b27dbc1742ec11371c6eea63a7a54418e7632b
Sarawut210747 <aok55677@gmail.com>
2026-02-07 00:24:02 +0700

    End_Week3

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        moveSpeed = 50;
+        moveSpeed = 10;
     }
 
     // Update is called once per frame
